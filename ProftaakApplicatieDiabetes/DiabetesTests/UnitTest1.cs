6893cf55c5820671db781391bc3650c0d8a4f7f1
TurtlSandwich <i398785@fhict.nl>
2019-09-04 10:39:14 +0200

    Made Project + Dependencies + Layers

---
+++ ProftaakApplicatieDiabetes/DiabetesTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiabetesTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
