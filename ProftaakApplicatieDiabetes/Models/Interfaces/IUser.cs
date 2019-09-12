0f89bbda78bde5a26ea04e09decd1f5829ddc5a8
Kohlen,JasperKohlen J <j.kohlen@student.fontys.nl>
2019-09-12 14:54:55 +0200

    Added Interfaces folders, Put gender and accounttype enum outside of the User class

---
+++ ProftaakApplicatieDiabetes/Models/Interfaces/IUser.cs
@@ -0,0 +1,24 @@
+﻿using Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public interface IUser
+    {
+
+        int BSN { get; }
+        string FirstName { get; }
+        string LastName { get; }
+        string Address { get; }
+        string City { get; }
+        string EmailAddress { get; }
+        DateTime BirthDate { get; }
+        Gender UserGender { get; }
+        AccountType UserAccountType { get; }
+        bool Status { get; }
+        string Password { get; }
+        User Doctor { get; }
+    }
+}
