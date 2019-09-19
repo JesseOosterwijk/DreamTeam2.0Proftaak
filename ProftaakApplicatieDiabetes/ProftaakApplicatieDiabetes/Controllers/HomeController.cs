93681cf4a4bb578a97a1124cf16292ea0d9d0301
Anuwat Kuiper <340421@student.fontys.nl>
2019-09-19 12:29:37 +0200

    Calculate

--- ProftaakApplicatieDiabetes/ProftaakApplicatieDiabetes/Controllers/HomeController.cs
+++ ProftaakApplicatieDiabetes/ProftaakApplicatieDiabetes/Controllers/HomeController.cs
@@ -15,11 +15,6 @@ namespace ProftaakApplicatieDiabetes.Controllers
             return View();
         }
 
-        public IActionResult Home()
-        {
-            return View();
-        }
-
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
