4ccaf1a06373ede9550c752d0866e5f4f47a87aa
Dennis Hong <340272@student.fontys.nl>
2019-09-19 11:24:00 +0200

    Finished skeleton for insuline calculation

---
+++ ProftaakApplicatieDiabetes/Logic/Interface/ICalculationLogic.cs
@@ -0,0 +1,7 @@
+﻿namespace Logic
+{
+    public interface ICalculationLogic
+    {
+        double CalculateMealtimeDose(double Weight, double TotalCarbs, double CurrentBloodSugar, double TargetBloodSugar);
+    }
+}
---
+++ ProftaakApplicatieDiabetes/ProftaakApplicatieDiabetes/ViewModels/CalcModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProftaakApplicatieDiabetes.Models
+{
+    public class CalcModel
+    {
+        public double Weight { get; set; }
+
+        public double TotalCarbs { get; set; }
+
+        public double CurrentBloodsugar { get; set; }
+
+        public double TargetBloodSugar { get; set; }
+    }
+}
