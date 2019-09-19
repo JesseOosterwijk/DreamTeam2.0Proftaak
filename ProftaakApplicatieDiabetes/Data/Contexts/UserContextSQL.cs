ea187d93285cfe65cb2e7b42386a19751008406f
TurtlSandwich <i398785@fhict.nl>
2019-09-19 10:14:04 +0200

    removed message

--- ProftaakApplicatieDiabetes/Data/Contexts/UserContextSQL.cs
+++ ProftaakApplicatieDiabetes/Data/Contexts/UserContextSQL.cs
@@ -6,6 +6,5 @@ namespace Data.Contexts
     public class UserContextSQL : IUserContext
     {
         private readonly SqlConnection _conn = Connection.GetConnection();
-        //Kut Jasper
     }
 }
