0afb717bb73ee020adebd8c4a3b3e2157a9aa5a7
TurtlSandwich <i398785@fhict.nl>
2019-09-25 11:42:31 +0200

    Added correct SqlConnection to UserContextSQL

--- ProftaakApplicatieDiabetes/Data/Contexts/UserContextSQL.cs
+++ ProftaakApplicatieDiabetes/Data/Contexts/UserContextSQL.cs
@@ -5,6 +5,6 @@ namespace Data.Contexts
 {
     public class UserContextSQL : IUserContext
     {
-        //private readonly SqlConnection _conn = Connection.GetConnection();
+        private readonly SqlConnection _conn = new SqlConnection(@"Data Source=mssql.fhict.local;Initial Catalog=dbi398785_diabetesdb;User ID=dbi398785_diabetesdb;Password=DreamTeam;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
     }
 }
