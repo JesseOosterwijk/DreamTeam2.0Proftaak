5b024f001e934f429f3de472952fb98f9ff5d842
TurtlSandwich <i398785@fhict.nl>
2019-09-26 14:02:31 +0200

    Update CalculationContextMemory.cs
    Test
    
    Co-Authored-By: Dennis Hong <dennis32100@users.noreply.github.com>

--- ProftaakApplicatieDiabetes/Data/Memory/CalculationContextMemory.cs
+++ ProftaakApplicatieDiabetes/Data/Memory/CalculationContextMemory.cs
@@ -10,7 +10,7 @@ namespace Data.Memory
     {
         public CalculationContextMemory()
         {
-            
+            //Test
         }
 
         public double CalculateMealtimeDose(double Weight, double TotalCarbs, double CurrentBloodSugar, double TargetBloodSugar)
