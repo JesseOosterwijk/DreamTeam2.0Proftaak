d3313f34cd737f1d5c3533a3b82799053a39d7fb
Dennis Hong <340272@student.fontys.nl>
2019-10-31 10:53:38 +0100

    Fixed userlogic interface

--- ProftaakApplicatieDiabetes/Logic/Interface/IUserLogic.cs
+++ ProftaakApplicatieDiabetes/Logic/Interface/IUserLogic.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Logic.Interface
 {
-    interface IUserLogic
+    public interface IUserLogic
     {
+        bool CheckIfUserAlreadyExists(string email);
+        bool CheckIfAccountIsActive(string email);
+        User GetUserInfo(string email);
+        User GetUserById(int userId);
+        bool CheckIfEmailIsValid(string userEmail);
+        User CheckValidityUser(string emailAddress, string password);
+        void CreateUser(User newUser);
     }
 }
