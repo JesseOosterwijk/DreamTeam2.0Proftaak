a4df9855ec6f74a5e791b5050f367f52d4a21df2
Mark Tempelman <43167879+MarkTempelman@users.noreply.github.com>
2019-11-21 13:55:21 +0100

    removed comments
    Co-authored-by: Anuwat Kuiper <340421@student.fontys.nl>

--- ProftaakApplicatieDiabetes/Data/Contexts/MessageContext.cs
+++ ProftaakApplicatieDiabetes/Data/Contexts/MessageContext.cs
@@ -13,35 +13,6 @@ namespace Data.Contexts
         public void SendMessage(MessageModel message)
         {
 
-            //try
-            //{
-            //    string query =
-            //        "INSERT INTO Message (SenderId, ReceiverId, Content, Title, DateOf) VALUES (@senderID, @receiverID, @content, @title, @dateOf)";
-
-            //    using (SqlCommand command = new SqlCommand(query, _conn))
-            //    {
-            //        _conn.Open();
-            //        command.Parameters.AddWithValue("@senderID", message.SenderId);
-            //        command.Parameters.AddWithValue("@receiverID", message.ReceiverId);
-            //        command.Parameters.AddWithValue("@content", message.Content);
-            //        command.Parameters.AddWithValue("@title", message.Title);
-            //        command.Parameters.AddWithValue("@dateOf", message.DateOfX);
-
-            //        command.ExecuteNonQuery();
-            //    }
-            //}
-            //catch (Exception)
-            //{
-            //    _conn.Close();
-            //    return false;
-            //}
-            //finally
-            //{
-            //    _conn.Close();
-            //}
-
-            //return true;
-
         }
 
         //TO BE REFACTORED
@@ -77,44 +48,6 @@ namespace Data.Contexts
 
             List<MessageModel> messages = new List<MessageModel>();
             return messages;
-            //try
-            //{
-            //    string query = "SELECT SenderId, ReceiverId, Content, Title, DateOf FROM Message WHERE SenderId = @senderID AND ReceiverId = @receiverID";
-
-            //    using (SqlCommand command = new SqlCommand(query, _conn))
-            //    {
-            //        _conn.Open();
-
-            //        command.Parameters.AddWithValue("@senderID", senderId);
-            //        command.Parameters.AddWithValue("@receiverID", receiverId);
-
-            //        SqlDataReader reader = command.ExecuteReader();
-            //        while (reader.Read())
-            //        {
-            //            MessageModel message = new MessageModel
-            //            {
-            //                SenderId = (int)reader["SenderID"],
-            //                ReceiverId = (int)reader["ReceiverId"],
-            //                Content = (string)reader["Content"],
-            //                Title = (string)reader["Title"],
-            //                DateOfX = (DateTime)reader["DateOf"]
-            //            };
-
-            //            messages.Add(message);
-            //        }
-            //        reader.Close();
-            //        return messages;
-            //    }
-            //}
-            //catch (Exception)
-            //{                
-            //    _conn.Close();
-            //    return messages;
-            //}
-            //finally
-            //{
-            //    _conn.Close();
-            //}          
         }
 
         public void StartChat(int senderId, int receiverId)
