a4cac0fbab0f2c9ec32229c13e14fdd27a3191ae
LuukPaans <47414476+LuukPaans@users.noreply.github.com>
2019-11-28 13:47:42 +0100

    Made MessageContextMemory
    Made MessageContextMemory

---
+++ ProftaakApplicatieDiabetes/DiabetesTests/MessageTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Memory;
+
+namespace DiabetesTests
+{
+    [TestClass]
+    public class MessageTests
+    {
+         
+    }
+}
